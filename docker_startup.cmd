edc8e00888a54cde08a5118b283a67a2f82db9a1
U-CAMBRIDGE\ricasiano <ricasiano@CUP-MNL-L115.ad.cambridge.org>
2018-04-24 18:07:34 +0800

    remove going to directory, unnecessary procedure and it's causing a path not found error

--- docker_startup.cmd
+++ docker_startup.cmd
@@ -1,6 +1,5 @@
 @echo off
 echo Running Docker. Please wait...
-d:
-cd DirPath\To\Your\Docker\Composer\Directory
+
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\DockerStartupLog.txt" 2>&1 
 PowerShell D:\Your\DirPath\To\PowerShell\File\docker_start.ps1 >> "%TEMP%\DockerStartupLog.txt" 2>&1
