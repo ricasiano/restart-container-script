4e9e6a9ee59c112b36aa997cf9fb17cabe6dca3e
Rai Icasiano <bos_tsip@yahoo.com>
2018-04-24 18:12:42 +0800

    Update docker_start.ps1

--- docker_start.ps1
+++ docker_start.ps1
@@ -1,6 +1,6 @@
 & "C:\Your\Path\To\Docker\Windows\Executable\Docker for Windows.exe"
 $containerName = 'your_container_name'
-$dockerServiceName = 'your_service_name_defined_in_docker-compose.yml'
+$dockerServiceName = 'your_service_name_defined_in_docker_compose_yml'
 $ServiceName = 'com.docker.service'
 $arrService = Get-Service -Name $ServiceName
 while ($arrService.Status -ne 'Running')
